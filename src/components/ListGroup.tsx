0074826db3a0798f48781d0f26613a5c88cb5432
oodoabel <get2abeloodo@gmail.com>
2024-09-19 22:48:40 -0700

    day_2

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,15 +1,33 @@
+import { useState } from "react";
+
 function ListGroup() {
   let items = ["New York", "Kano", "Maidguri", "Minna", "Tokyo", "Paris"];
-  items = [];
+
+  // let selectedItem = 0;
+  const [selectedIndex, setSelectedIndex] = useState(-1); //hook
+  // arr[0];
+  // arr[1];
+
   return (
     <>
       <h1>Lists</h1>
-      {items.length === 0 ? "No item found" : null}
+      {items.length === 0 && <p>Item not found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              index === selectedIndex
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+            key={item}
+          >
+            {item}
+          </li>
         ))}
-        ;
       </ul>
     </>
   );
